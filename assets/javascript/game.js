c2558b29550c04990b2aa4278aaa0208b61c48c3
Tomoyuki Komuro <Tomo Komuro@tomos-macbook-pro-2.local>
2018-03-19 23:11:13 -0700

    more edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //Keep on getting an error saying this all Jquery functions are not defined, even though I pasted the source link on my html.
 
 var wins = 0;
 var losses = 0;
@@ -9,9 +10,25 @@ var redCrystal = Math.floor((Math.random() * 12) + 1);
 var blueCrystal = Math.floor((Math.random() * 12) + 1);
 var yellowCrystal = Math.floor((Math.random() * 12) + 1);
 var greenCrystal = Math.floor((Math.random() * 12) + 1);
+// generates random numbers from 19 to 120 for the total score.
+// generates random numbers from 1 to 12 for each crystals.
    
 $('.wins').text(wins);
 $('.losses').text(losse);
+$('#randomNumber').text(random);
+
+function myFunction(){
+    alert("You won!!");
+        wins++; 
+        $('#Wins').text(wins);
+        reset();
+}
+function lose(){
+    alert ("You lose!!");
+      losses++;
+      $('#losses').text(losses);
+      reset()
+}
 
 var restart = function () {
     start = 0;
@@ -23,22 +40,24 @@ $('#red').click(function(){
     totalScore = totalScore + redCrystal;
     console.log("New totalScore= " + totalScore);
     $('#userTotal').text(totalScore); 
-    if (totalScore === Random){
+    if (totalScore === random){
         win();
     }
-    else if ( totalScore > Random){
+    else if (totalScore > random){
         lose();
         }
     })
+// couldn't get each button to work.
+// if the total score equals to the random number generated at the start of the game, user wins.
 
 $('#blue').click(function(){
     totalScore = totalScore + blueCrystal;
     console.log("New totalScore= " + totalScore);
 $('#userTotal').text(totalScore); 
-    if (totalScore === Random){
+    if (totalScore === random){
         win();
             }
-        else if ( totalScore > Random){
+        else if (totalScore > Random){
         lose();
         }
     })
@@ -48,10 +67,10 @@ $('#yellow').click(function(){
     totalScore = totalScore + yellowCrystal;
      console.log("New totalScore= " + totalScore);
     $('#userTotal').text(totalScore);        
-     if (totalScore === Random){
+     if (totalScore === random){
         win();
         }
-    else if ( totalScore > Random){
+    else if (totalScore > random){
         lose();
         }
     })
@@ -60,26 +79,15 @@ $('#green').click(function(){
     totalScore = totalScore + greenCrystal;
     console.log("New totalScore= " + totalScore);
     $('#userTotal').text(totalScore);        
-    if (totalScore === Random){
+    if (totalScore === random){
         win();
         }
-    else if ( totalScore > Random){
+    else if (totalScore > random){
     lose();
         }
     })
 
-    function myFunction(){
-        alert("You won!!");
-            wins++; 
-            $('#Wins').text(wins);
-            reset();
-    }
-    function lose(){
-        alert ("You lose!!");
-          losses++;
-          $('#losses').text(losses);
-          reset()
-    }
+
 
 
 })
